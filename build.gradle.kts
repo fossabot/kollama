ab38ee4b281baaba76e109a7d9e4fb726b8a5661
MojaveHao <HTTcode2020@126.com>
2026-02-22 12:04:00 +0800

    feat: init project

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
     kotlin("jvm") version "2.3.0"
     id("org.jetbrains.dokka") version "2.1.0"
     kotlin("plugin.serialization") version "2.2.20"
-    `java-library`
-    `maven-publish`
-    signing
 }
 
 group = "net.blophy.nova"
@@ -43,66 +40,7 @@ tasks.test {
 
 // 生成源码 jar
 tasks.withType<Jar> {
-    // 可选：如果希望源码包也包含 Kotlin 源码
     from(sourceSets.main.get().allSource)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            pom {
-                name.set("kollama")
-                description.set("A Kotlin-First Ollama Client with Full Type Safety")
-                url.set("https://github.com/BlophyNova/kollama")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("MojaveHao")
-                        name.set("MojaveHao")
-                        email.set("MojaveHao")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/BlophyNova/kollama.git")
-                    developerConnection.set("scm:git:ssh://github.com:BlophyNova/kollama.git")
-                    url.set("https://github.com/BlophyNova/kollama")
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-
-            credentials {
-                username = System.getenv("MVNC_USERNAME")
-                password = System.getenv("MVNC_PASSWORD")
-            }
-        }
-    }
-}
-
-signing {
-    val keyId = System.getenv("SIGNING_KEY_ID")
-    val password = System.getenv("SIGNING_PASSWORD")
-
-    if (keyId != null && password != null) {
-        useInMemoryPgpKeys(keyId, password)
-    }
-    sign(publishing.publications["mavenJava"])
 }
 
 dokka {
